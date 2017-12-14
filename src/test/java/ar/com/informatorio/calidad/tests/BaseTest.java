d12c87d22894b62f5acd9389d7e591c8f9fa77af
Jonatan Schenfeld <schenfeld.jonatan@gmail.com>
2017-12-14 00:34:43 -0300

    lesson7: TestNG Suite and  parameters annotation

--- src/test/java/ar/com/informatorio/calidad/tests/BaseTest.java
+++ src/test/java/ar/com/informatorio/calidad/tests/BaseTest.java
@@ -2,20 +2,37 @@ package ar.com.informatorio.calidad.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
 
 public class BaseTest {
 
 	protected WebDriver driver;
 
 	@BeforeMethod
-	public void setUp() {
-		ChromeDriverManager.getInstance().setup();
-		driver = new ChromeDriver();
+	@Parameters(value = { "browser", "url" })
+	public void setUp(@Optional String browser, @Optional String url) {
+		switch (browser) {
+		case "chrome":
+			ChromeDriverManager.getInstance().setup();
+			driver = new ChromeDriver();	
+			break;
+		case "firefox":
+			FirefoxDriverManager.getInstance().setup();
+			driver = new FirefoxDriver();
+			break;
+		default:
+			throw new IllegalArgumentException("Browser " + browser + " not supported");
+		}
+		driver.get(url);
 	}
+
 
 	@AfterMethod
 	public void tearDown() {
